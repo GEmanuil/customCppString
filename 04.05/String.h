d86d35ab1955c53afd0c001d767a5af5e35732d6
GEmanuil <99758663+GEmanuil@users.noreply.github.com>
2024-04-08 15:00:21 +0300

    Update String.h
    better string

--- 04.05/String.h
+++ 04.05/String.h
@@ -1,22 +1,30 @@
 #pragma once
+#include <iostream>
+
 class String
 {
 public:
 	String();
 	String(const char* arr);
-	String(const String& str);
+	String(const String& other);
 	String& operator=(const String& other);
+	void operator+=(const String& other);
+	String operator+(const String& one) const;
 	~String();
 
-	void setLenght(size_t);
-	void copy(const char* arr);
-	void concat(const char* arr);
-	void erase();
-	void print();
 
-	size_t getLenght();
+	const int getStringSize() const;
+
+	//using this to access the string when the object String is const
+	const char& operator[](const int& n) const;
+	//using this as a lvalue
+	char& operator[](const int& n);
+
+
 private:
-	char* str;
-	size_t lenght;
+	char* string;
+	size_t stringSize;
+
+	void free();
 };
 
